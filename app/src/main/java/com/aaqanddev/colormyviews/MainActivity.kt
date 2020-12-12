cbb35015f9bb465fabd0d54d7eecf26f87c26c59
aaq-and-dev <aaronquaday@gmail.com>
2020-12-12 15:20:43 -0800

    refactors to create lists of functions just once, passing list and view to makeColored()

--- app/src/main/java/com/aaqanddev/colormyviews/MainActivity.kt
+++ app/src/main/java/com/aaqanddev/colormyviews/MainActivity.kt
@@ -7,37 +7,33 @@ import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setListeners()
     }
 
-    private fun makeColored(view: View){
+    private fun makeColored(view: View, list :List<(View)->Unit>){
 
-        println({view.setBackgroundColor(Color.GRAY)}.javaClass)
-        println({view.setBackgroundResource(R.drawable.ic_baseline_arrow_right_alt_24)}.javaClass)
-        when (view.id){
-            R.id.box_one_text -> { val oneFunList =
-                listOf<()->Unit>({ view.setBackgroundResource(R.drawable.ic_baseline_accessible_forward_24) },
-                    {view.setBackgroundColor(Color.YELLOW)},{ view.setBackgroundColor(Color.DKGRAY) });
-                oneFunList.random().invoke()}
-            R.id.box_two_text -> { val twoFunList =
-                listOf({view.setBackgroundColor(Color.GRAY)}, {view.setBackgroundColor(Color.CYAN)},
-                    {view.setBackgroundResource(R.drawable.ic_baseline_adb_24)});
-                twoFunList.random().invoke()}
-            R.id.box_three_text -> { val threeFunList =
-                listOf({view.setBackgroundColor(Color.BLUE)}, {view.setBackgroundColor(Color.BLACK)},
-                    {view.setBackgroundResource(R.drawable.ic_baseline_add_reaction_24)});
-                threeFunList.random().invoke()}
-            R.id.box_four_text -> {val fourFunList = listOf({view.setBackgroundColor(Color.MAGENTA)},
-                {view.setBackgroundColor(Color.GREEN)}, {view.setBackgroundResource(R.drawable.ic_baseline_speaker_24)});
-                fourFunList.random().invoke()}
-            R.id.box_five_text -> { listOf({view.setBackgroundColor(Color.BLUE)}, {view.setBackgroundColor(Color.RED)},
-                {view.setBackgroundResource(R.drawable.ic_baseline_arrow_right_alt_24)}).random().invoke()}
-            else -> view.setBackgroundColor(Color.LTGRAY)
-        }
+        //println({view.setBackgroundColor(Color.GRAY)}.javaClass)
+        //println({view.setBackgroundResource(R.drawable.ic_baseline_arrow_right_alt_24)}.javaClass)
+//        when (view.id){
+//            R.id.box_one_text -> {
+//
+//                list.random().invoke(view)}
+//            R.id.box_two_text -> {
+//
+//                list.random().invoke(view)}
+//            R.id.box_three_text -> {
+//
+//                list.random().invoke(view)}
+//            R.id.box_four_text -> {
+//                list.random().invoke(view)}
+//            R.id.box_five_text -> { list.random().invoke(view)}
+//            else -> view.setBackgroundColor(Color.LTGRAY)
+//        }
+        list.random().invoke(view)
     }
 
     private fun setListeners(){
@@ -52,8 +48,17 @@ class MainActivity : AppCompatActivity() {
             listOf(boxOneText, boxTwoTextView, boxThreeTextView, boxFourTextView,
                 boxFiveTextView, rootConstraintLayout)
 
-        for (view in clickableViews){
-            view.setOnClickListener{makeColored(view)}
+        val listOfListOfFunctions : List<List<(View)->Unit>> = listOf(listOf<(View) -> Unit>({ it.setBackgroundResource(R.drawable.ic_baseline_accessible_forward_24) },
+            {it.setBackgroundColor(Color.YELLOW)},{ it.setBackgroundColor(Color.DKGRAY) }), listOf<(View)->Unit>({it.setBackgroundColor(Color.BLUE)}, {it.setBackgroundColor(Color.BLACK)},
+            {it.setBackgroundResource(R.drawable.ic_baseline_add_reaction_24)}), listOf<(View)->Unit>( {it.setBackgroundColor(Color.GRAY)}, {it.setBackgroundColor(Color.CYAN)},
+            {it.setBackgroundResource(R.drawable.ic_baseline_adb_24)}  ), listOf<(View)->Unit>({it.setBackgroundColor(Color.MAGENTA)},
+            {it.setBackgroundColor(Color.GREEN)}, {it.setBackgroundResource(R.drawable.ic_baseline_speaker_24)}), listOf({it.setBackgroundColor(Color.BLUE)}, {it.setBackgroundColor(Color.RED)},
+            {it.setBackgroundResource(R.drawable.ic_baseline_arrow_right_alt_24)}), listOf({it.setBackgroundColor(Color.LTGRAY)}, {it.setBackgroundColor(Color.CYAN)}))
+
+        for ((i, view) in clickableViews.withIndex()){
+
+            view.setOnClickListener{makeColored(view, listOfListOfFunctions[i])}
         }
+
     }
 }
